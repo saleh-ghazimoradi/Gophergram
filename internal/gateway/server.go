8587f0a1b30460d63321de5887359eaf86c6643a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-10-22 11:58:05 +0330

    add graceful shutdown to the project

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -1,10 +1,15 @@
 package gateway
 
 import (
+	"context"
+	"errors"
 	"github.com/saleh-ghazimoradi/Gophergram/config"
 	"github.com/saleh-ghazimoradi/Gophergram/docs"
+	"github.com/saleh-ghazimoradi/Gophergram/logger"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"log"
 	"net/http"
 	"time"
 )
@@ -23,8 +28,32 @@ func Server(mux http.Handler) error {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	shutdown := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		logger.Logger.Infow("signal caught", "signal", s.String())
+
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
+	err := srv.ListenAndServe()
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
 	}
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+
+	logger.Logger.Infow("server has stopped", "addr", config.AppConfig.General.Listen, "env", config.AppConfig.Env.Env)
+
 	return nil
 }
