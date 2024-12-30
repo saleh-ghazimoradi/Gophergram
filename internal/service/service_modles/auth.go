a0dbde1a411d7aa8b3e453542136e351d3626696
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-30 21:47:32 +0330

    improve the docker compose file

--- internal/service/service_modles/auth.go
+++
@@ -1,7 +0,0 @@
-package service_modles
-
-type RegisterUserPayload struct {
-	Username string `json:"username" validate:"required,max=100"`
-	Email    string `json:"email" validate:"required,email,max=255"`
-	Password string `json:"password" validate:"required,min=3,max=72"`
-}
--- internal/service/service_modles/followers.go
+++
@@ -1,9 +0,0 @@
-package service_modles
-
-import "time"
-
-type Followers struct {
-	UserID     int64     `json:"user_id"`
-	FollowerID int64     `json:"follower_id"`
-	CreatedAt  time.Time `json:"created_at"`
-}
