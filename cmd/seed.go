a0560416a9d7d790c297b1fdeceda58cf7867c19
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-30 21:49:41 +0330

    add some files to the cmd directory using cobra cli

--- cmd/seed.go
+++ cmd/seed.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,7 @@ import (
 // seedCmd represents the seed command
 var seedCmd = &cobra.Command{
 	Use:   "seed",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Seeding DB",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("seed called")
 	},
@@ -28,13 +21,4 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(seedCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// seedCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// seedCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
