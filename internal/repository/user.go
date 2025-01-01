1f7c558b20f02de209badf85caa05812175ab2ca
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-01 15:06:47 +0330

    implement user creation functionality in the repository layer

---
+++ internal/repository/user.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"github.com/saleh-ghazimoradi/Gophergram/internal/service/service_models"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *service_models.User) error
+	WithTx(tx *sql.Tx) UserRepository
+}
+
+type userRepository struct {
+	dbRead  *sql.DB
+	dbWrite *sql.DB
+	tx      *sql.Tx
+}
+
+func (u *userRepository) Create(ctx context.Context, user *service_models.User) error {
+
+	query := `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id, created_at`
+
+	args := []any{user.Username, user.Password, user.Email}
+	if err := u.dbWrite.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *userRepository) WithTx(tx *sql.Tx) UserRepository {
+	return &userRepository{
+		dbRead:  u.dbRead,
+		dbWrite: u.dbWrite,
+		tx:      tx,
+	}
+}
+
+func NewUserRepository(dbRead, dbWrite *sql.DB) UserRepository {
+	return &userRepository{
+		dbRead:  dbRead,
+		dbWrite: dbWrite,
+	}
+}
