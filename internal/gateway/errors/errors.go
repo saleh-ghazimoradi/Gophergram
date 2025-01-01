5354097bc6701968ea9fa8e3f29f0285f09d41f9
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-01 22:31:40 +0330

    create some helper functions for the errors to keep us at ease

---
+++ internal/gateway/errors/errors.go
@@ -0,0 +1,49 @@
+package errors
+
+import (
+	"github.com/saleh-ghazimoradi/Gophergram/internal/gateway/json"
+	"github.com/saleh-ghazimoradi/Gophergram/logger"
+	"net/http"
+)
+
+func InternalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Error("internal error", "method", r.Method, "path", r.URL.Path, "err", err.Error())
+	json.WriteJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func BadRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warn("bad request", "method", r.Method, "path", r.URL.Path, "err", err.Error())
+	json.WriteJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func NotFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warn("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	json.WriteJSONError(w, http.StatusNotFound, err.Error())
+}
+
+func ConflictResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Error("conflict response: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	json.WriteJSONError(w, http.StatusConflict, err.Error())
+}
+
+func unauthorizedErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warn("unauthorized error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+	json.WriteJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func unauthorizedBasicErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warn("unauthorized basic error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+	json.WriteJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func forbiddenResponse(w http.ResponseWriter, r *http.Request) {
+	logger.Logger.Warn("forbidden", "method", r.Method, "path", r.URL.Path, "error")
+	json.WriteJSONError(w, http.StatusForbidden, "forbidden")
+}
+
+func rateLimitExceededResponse(w http.ResponseWriter, r *http.Request, retryAfter string) {
+	logger.Logger.Warn("rate limit exceeded", "method", r.Method, "path", r.URL.Path)
+	w.Header().Set("Retry_After", retryAfter)
+	json.WriteJSONError(w, http.StatusTooManyRequests, "rate limit exceeded, retry after: "+retryAfter)
+}
