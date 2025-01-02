7fab6599dd42a79840c4f91bda036c0019a0e8fe
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-02 12:01:57 +0330

    add comment model to the project

---
+++ internal/service/service_models/comment.go
@@ -0,0 +1,12 @@
+package service_models
+
+import "time"
+
+type Comment struct {
+	ID        int64     `json:"id"`
+	PostID    int64     `json:"post_id"`
+	UserID    int64     `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	User      User      `json:"user"`
+}
