af8443b2b1efc41785f1dc2409253acafec3900c
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-02 16:15:54 +0330

    implement post CRUD functionalites in the service layer

---
+++ internal/service/post.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"github.com/saleh-ghazimoradi/Gophergram/internal/repository"
+	"github.com/saleh-ghazimoradi/Gophergram/internal/service/service_models"
+	"github.com/saleh-ghazimoradi/Gophergram/utils"
+)
+
+type PostService interface {
+	Create(ctx context.Context, post *service_models.Post) error
+	GetById(ctx context.Context, id int64) (*service_models.Post, error)
+	Update(ctx context.Context, post *service_models.Post) error
+	Delete(ctx context.Context, id int64) error
+}
+
+type postService struct {
+	postRepo repository.PostRepository
+	db       *sql.DB
+}
+
+func (p *postService) Create(ctx context.Context, post *service_models.Post) error {
+	return utils.WithTransaction(ctx, p.db, func(tx *sql.Tx) error {
+		userRepoWithTx := p.postRepo.WithTx(tx)
+		return userRepoWithTx.Create(ctx, post)
+	})
+}
+
+func (p *postService) GetById(ctx context.Context, id int64) (*service_models.Post, error) {
+	return p.postRepo.GetById(ctx, id)
+}
+
+func (p *postService) Update(ctx context.Context, post *service_models.Post) error {
+	return p.postRepo.Update(ctx, post)
+}
+
+func (p *postService) Delete(ctx context.Context, id int64) error {
+	return p.postRepo.Delete(ctx, id)
+}
+
+func NewPostService(postRepo repository.PostRepository, db *sql.DB) PostService {
+	return &postService{
+		postRepo: postRepo,
+		db:       db,
+	}
+}
