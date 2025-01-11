ae1ba6925e0ee8887de2b01fec2ebbbf6b83ef45
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-11 12:51:51 +0330

    remove the read user id param from the helper directory

--- internal/gateway/helper/helper.go
+++ internal/gateway/helper/helper.go
@@ -16,15 +16,6 @@ func ReadIdParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func ReadUserIdParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("userID"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id")
-	}
-	return id, nil
-}
-
 func ReadTokenParam(r *http.Request) (string, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	token := params.ByName("token")
