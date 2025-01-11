f50460cac36148bffe241a4ac0858275e4a2da1e
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-11 12:53:05 +0330

    remove the middleware from the user routes

--- internal/gateway/routes/user.go
+++ internal/gateway/routes/user.go
@@ -13,7 +13,7 @@ func registerUserRoutes(router *httprouter.Router, user *handlers.UserHandler, m
 	router.Handler(http.MethodGet, "/v1/users/:id", authTokenMiddleware(http.HandlerFunc(user.GetUserHandler)))
 	router.Handler(http.MethodPut, "/v1/users/:id/follow", authTokenMiddleware(http.HandlerFunc(user.FollowUserHandler)))
 	router.Handler(http.MethodPut, "/v1/users/:id/unfollow", authTokenMiddleware(http.HandlerFunc(user.UnFollowUserHandler)))
-
 	router.Handler(http.MethodGet, "/v1/user/feed", authTokenMiddleware(http.HandlerFunc(feed.GetUserFeedHandler)))
+
 	router.HandlerFunc(http.MethodPut, "/v1/user/activate/:token", user.ActivateUserHandler)
 }
