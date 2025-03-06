5becfd6ede5ded532c040daaec563af70c37a80b
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-06 11:29:12 +0330

    add user model to the project

---
+++ internal/service/service_models/users.go
@@ -0,0 +1,11 @@
+package service_models
+
+import "time"
+
+type Users struct {
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
